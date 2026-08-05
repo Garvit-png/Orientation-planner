8b222f91066acffcc0cdb8700f425ebc51e12dad
Garvit Gandhi <garvitgandhi0313@gmail.com>
2026-08-05 15:01:02 +0530

    fix: remove unused Link import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Routes, Route, useNavigate, Link } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import EventSchedule from './components/EventSchedule';
 
 function Home() {
